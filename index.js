aac2888668fbc4d8168ee9da869f8efb31418e5f
Sahil Abrar <zeroxkhan2@gmail.com>
2023-10-21 19:37:25 +0600

    Did some adjustment of patch, post, get

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
-const cors = require('cors');
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,36 +26,28 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const brandCollection = client.db('brandDB').collection('brand');
-    const brandUserCollection = client.db('brandDB').collection('userBrands');
-    const NewBrandCollection = client.db('brandDB').collection('newBrand');
-    const allProductCollection = client.db('brandDB').collection('allProducts');
-    const cartCollection = client.db('brandDB').collection('cart');
-    const userCollection = client.db('brandDB').collection('user');
+    const brandCollection = client.db("brandDB").collection("brand");
+    const brandUserCollection = client.db("brandDB").collection("userBrands");
+    const NewBrandCollection = client.db("brandDB").collection("newBrand");
+    const allProductCollection = client.db("brandDB").collection("allProducts");
+    const cartCollection = client.db("brandDB").collection("cart");
+    const userCollection = client.db("brandDB").collection("user");
 
     // Old json data infos
-    app.get('/brands', async (req, res) => {
+    app.get("/brands", async (req, res) => {
       const cursor = brandCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
     // Main 6 types data json
-    app.get('/newBrand', async (req, res) => {
+    app.get("/newBrand", async (req, res) => {
       const cursor = NewBrandCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    // Data of add product form and added products page cards
-    // app.get('/userBrands', async (req, res) => {
-    //   const email = req.query.email;
-    //   const query = { email: email };
-    //   const result = await brandUserCollection.find(query);
-    //   res.send(result);
-    // });
-
-    app.get('/userBrands', async (req, res) => {
+    app.get("/userBrands", async (req, res) => {
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
@@ -64,21 +56,30 @@ async function run() {
 
       res.send(result);
     });
+    app.get("/cart", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await cartCollection.find(query).toArray();
 
-    app.get('/user', async (req, res) => {
+      res.send(result);
+    });
+
+    app.get("/user", async (req, res) => {
       const cursor = userCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.get('/cart', async (req, res) => {
+    app.get("/cart", async (req, res) => {
       const cursor = cartCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
     //
-    app.get('/userBrands/:id', async (req, res) => {
+    app.get("/userBrands/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await brandUserCollection.findOne(query);
@@ -86,7 +87,7 @@ async function run() {
     });
 
     // Update get and patch
-    app.get('/updateItem/:id', async (req, res) => {
+    app.get("/updateItem/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await brandUserCollection.findOne(query);
@@ -94,15 +95,16 @@ async function run() {
     });
 
     // Update get and patch
-    app.patch('/updateItem/:id', async (req, res) => {
-      const id = req.params.id;
+    app.patch("/updateItem/:id", async (req, res) => {
+      const options = { upsert: true };
       const data = req.body;
+      const id = req.params.id;
       const updateDoc = {
         $set: {
           name: data.name,
+          email: data.email,
           price: data.price,
           type: data.type,
-          email: data.email,
           brandName: data.brandName,
           description: data.description,
           image: data.image,
@@ -112,61 +114,36 @@ async function run() {
         {
           _id: new ObjectId(id),
         },
-        updateDoc
+        updateDoc,
+        options
       );
       res.send(result);
     });
-
-    // Different products under brand name related data
-    app.get('/allProducts', async (req, res) => {
-      const cursor = allProductCollection.find();
-      const result = await cursor.toArray();
+    app.patch("/customCart/:id", async (req, res) => {
+      const options = { upsert: true };
+      const data = req.body;
+      const id = req.params.id;
+      const updateDoc = {
+        $set: {
+          name: data.name,
+          email: data.email,
+          price: data.price,
+          type: data.type,
+          brandName: data.brandName,
+          description: data.description,
+          image: data.image,
+        },
+      };
+      const result = await cartCollection.updateOne(
+        {
+          _id: new ObjectId(id),
+        },
+        updateDoc,
+        options
+      );
       res.send(result);
     });
-
-    // Different products under brand name related data
-    app.post('/allProducts', async (req, res) => {
-      const allProducts = req.body;
-      console.log(allProducts);
-      const result = await allProductCollection.insertOne(allProducts);
-      res.send(result);
-    });
-
-    // Old json data infos
-    app.post('/brands', async (req, res) => {
-      const allBrands = req.body;
-      console.log(allBrands);
-      const result = await brandCollection.insertOne(allBrands);
-      res.send(result);
-    });
-
-    // Cart data
-    app.post('/cart', async (req, res) => {
-      const product = req.body;
-      const query = { id: product.id };
-      const existingProduct = await cartCollection.findOne(query);
-
-      if (existingProduct) {
-        // Product already exists, set 400 status and send a JSON response
-        return res
-          .status(400)
-          .json({ message: 'Product already exists in the cart' });
-      } else {
-        // Product doesn't exist, add it to the cart and set 200 status
-        const result = await cartCollection.insertOne(product);
-        res.status(200).json({ message: 'Product added to cart' });
-      }
-    });
-
-    // Data of add product form and added products page cards
-    app.post('/userBrands', async (req, res) => {
-      const brandUsers = req.body;
-      console.log(brandUsers);
-      const result = await brandUserCollection.insertOne(brandUsers);
-      res.send(result);
-    });
-
-    app.put('/userBrands/:id', async (req, res) => {
+    app.put("/userBrands/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
@@ -185,8 +162,62 @@ async function run() {
       res.send(result);
     });
 
+    // Different products under brand name related data
+    app.get("/allProducts", async (req, res) => {
+      const cursor = allProductCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Different products under brand name related data
+    app.post("/allProducts", async (req, res) => {
+      const allProducts = req.body;
+      console.log(allProducts);
+      const result = await allProductCollection.insertOne(allProducts);
+      res.send(result);
+    });
+
+    // Old json data infos
+    app.post("/brands", async (req, res) => {
+      const allBrands = req.body;
+      console.log(allBrands);
+      const result = await brandCollection.insertOne(allBrands);
+      res.send(result);
+    });
+
+    // Cart data
+    app.post("/cart", async (req, res) => {
+      const product = req.body;
+      const query = { id: product.id };
+      const existingProduct = await cartCollection.findOne(query);
+
+      if (existingProduct) {
+        // Product already exists, set 400 status and send a JSON response
+        return res
+          .status(400)
+          .json({ message: "Product already exists in the cart" });
+      } else {
+        // Product doesn't exist, add it to the cart and set 200 status
+        const result = await cartCollection.insertOne(product);
+        res.status(200).json({ message: "Product added to cart" });
+      }
+    });
+    app.post("/customCart", async (req, res) => {
+      const product = req.body;
+      const result = await cartCollection.insertOne(product);
+      res.send(result);
+    });
+
+    // Data of add product form and added products page cards
+    app.post("/userBrands", async (req, res) => {
+      const brandUsers = req.body;
+      console.log(brandUsers);
+      const result = await brandUserCollection.insertOne(brandUsers);
+      res.send(result);
+    });
+
     // adding new user
-    app.post('/user', async (req, res) => {
+    app.post("/user", async (req, res) => {
       const newUser = req.body;
       const query = { email: newUser.email };
       const existingUser = await userCollection.findOne(query);
@@ -198,44 +229,24 @@ async function run() {
     });
 
     // Added products deletation related data
-    app.delete('/userBrands/:id', async (req, res) => {
+    app.delete("/userBrands/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await brandUserCollection.deleteOne(query);
       res.send(result);
     });
 
-    // cart deletation related data
-    // app.delete('/cart/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await cartCollection.deleteOne(query);
-    //   res.send(result);
-    // });
-
-    app.delete('/cart/:id', async (req, res) => {
-      try {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await cartCollection.deleteOne(query);
-
-        if (result.deletedCount === 1) {
-          // Item deleted successfully
-          res.status(204).send(); // 204 No Content
-        } else {
-          // Item not found or not deleted
-          res.status(404).json({ message: 'Item not found or not deleted' });
-        }
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
-      }
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 });
+    await client.db("admin").command({ ping: 1 });
     console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
+      "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
@@ -244,8 +255,8 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Brand Shop server is running perfectly');
+app.get("/", (req, res) => {
+  res.send("Brand Shop server is running perfectly");
 });
 
 app.listen(port, () => {
